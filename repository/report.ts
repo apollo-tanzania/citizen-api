54481bc5d0e229a65978f975e44eabb18fdabc07
masakachristopher <masakachristopher@yahoo.com>
2023-04-27 23:54:01 +0300

    modify create report logic

--- repository/report.ts
+++ repository/report.ts
@@ -3,7 +3,7 @@ import ReportModel from '../model/report';
 import { PatchReportDto } from '../dto/report/patchReport';
 import { PutReportDto } from '../dto/report/putReport';
 import { CreateReportDto } from '../dto/report/createReport';
-import { ClientSession } from 'mongoose';
+import { ClientSession, UpdateQuery } from 'mongoose';
 import mongooseService from '../common/services/mongoose.service';
 import PhoneModel from '../model/phone';
 import StolenPhoneModel from '../model/stolenPhone';
@@ -20,6 +20,78 @@ class ReportRepository {
         log('Created new instance of Report Repository');
     }
 
+    // async addReport(reportFields: CreateReportDto) {
+    //     const { startSession } = mongooseService.getMongoose();
+    //     const session: ClientSession = await startSession();
+
+    //     session.startTransaction(); // Start transaction
+
+
+    //     try {
+
+    //         let queryFilterArray = []
+    //         type QueryType = Record<any, Number>
+    //         let query: QueryType = {}
+
+    //         if (reportFields.phone.imei1) {
+    //             query.imei1 = reportFields.phone.imei1
+    //             queryFilterArray.push({ "phone.imei1": query.imei1 })
+    //         }
+    //         if (reportFields.phone.imei2) {
+    //             query.imei2 = reportFields.phone.imei2
+    //             queryFilterArray.push({ "phone.imei2": query.imei2 })
+    //         }
+
+    //         if (reportFields.phone.imei3) {
+    //             query.imei3 = reportFields.phone.imei3
+    //             queryFilterArray.push({ "phone.imei3": query.imei3 })
+    //         }
+
+    //         // returns null if not found
+    //         const reportFound = await this.Report.findOne({
+    //             $or: queryFilterArray,
+    //         },
+    //             null,
+    //             { sort: { _id: -1 } }
+    //         ).exec();
+
+    //         let report;
+
+    //         report = new this.Report({
+    //             ...reportFields,
+    //         });
+
+    //         if (reportFound?._id) {
+    //             report = new this.Report({
+    //                 ...reportFields,
+    //                 originalReportId: mongooseService.getMongoose().Types.ObjectId(reportFound?._id),
+    //             });
+    //         }
+
+    //         await report.save();
+
+    //         const stolenPhone = new this.StolenPhone({
+    //             ...reportFields?.phone
+    //         })
+
+    //         await stolenPhone.save();
+
+    //         await session.commitTransaction();
+
+    //         return report;
+
+    //     } catch (error) {
+
+    //         await session.abortTransaction();
+    //         return error;
+
+    //     } finally {
+    //         session.endSession();
+
+    //     }
+    //     // await report.save();
+    //     // return reportId;
+    // }
     async addReport(reportFields: CreateReportDto) {
         const { startSession } = mongooseService.getMongoose();
         const session: ClientSession = await startSession();
@@ -29,27 +101,31 @@ class ReportRepository {
 
         try {
 
-            let queryFilterArray = []
+            let reportQModelueryFilterArray = []
+            let phoneModelQueryFilterArray = []
             type QueryType = Record<any, Number>
             let query: QueryType = {}
 
             if (reportFields.phone.imei1) {
                 query.imei1 = reportFields.phone.imei1
-                queryFilterArray.push({ "phone.imei1": query.imei1 })
+                reportQModelueryFilterArray.push({ "phone.imei1": query.imei1 })
+                phoneModelQueryFilterArray.push({ "imei1": query.imei2 })
             }
             if (reportFields.phone.imei2) {
                 query.imei2 = reportFields.phone.imei2
-                queryFilterArray.push({ "phone.imei2": query.imei2 })
+                reportQModelueryFilterArray.push({ "phone.imei2": query.imei2 })
+                phoneModelQueryFilterArray.push({ "imei2": query.imei2 })
             }
 
             if (reportFields.phone.imei3) {
                 query.imei3 = reportFields.phone.imei3
-                queryFilterArray.push({ "phone.imei3": query.imei3 })
+                reportQModelueryFilterArray.push({ "phone.imei3": query.imei3 })
+                phoneModelQueryFilterArray.push({ "imei3": query.imei3 })
             }
 
             // returns null if not found
             const reportFound = await this.Report.findOne({
-                $or: queryFilterArray,
+                $or: reportQModelueryFilterArray,
             },
                 null,
                 { sort: { _id: -1 } }
@@ -70,11 +146,30 @@ class ReportRepository {
 
             await report.save();
 
-            const stolenPhone = new this.StolenPhone({
-                ...reportFields?.phone
-            })
+            const existingPhone = await this.StolenPhone.findOne({
+                $or: phoneModelQueryFilterArray,
+            },
+                null,
+                { sort: { _id: -1 } }
+            ).exec();
 
-            await stolenPhone.save();
+            const update: UpdateQuery<{ countReportedStolenOrLost: number }> = { $inc: { countReportedStolenOrLost: 1 } }
+
+            if (existingPhone) {
+                await this.StolenPhone.findOneAndUpdate({
+                    $or: phoneModelQueryFilterArray
+                }, update)
+
+            } else {
+                const stolenPhone = new this.StolenPhone({
+                    ...reportFields?.phone,
+                    countReportedStolenOrLost: 1
+                })
+
+                await stolenPhone.save();
+
+            }
+
 
             await session.commitTransaction();
 
@@ -89,8 +184,6 @@ class ReportRepository {
             session.endSession();
 
         }
-        // await report.save();
-        // return reportId;
     }
 
     async removeReportById(reportId: string) {
