2e9dffb9c342079b74669ea438bdd34cae8b3699
masakachristopher <masakachristopher@yahoo.com>
2023-04-29 13:10:53 +0300

    modify controller

--- controller/permissionLog.ts
+++ controller/permissionLog.ts
@@ -1,14 +1,23 @@
 import express from 'express';
 import permissionLogService from '../service/permissionLog';
 import debug from 'debug';
+import extractParamsFromQuery from '../common/helpers/utils';
 
 const log: debug.IDebugger = debug('app:permission-log-controller');
 
 class PermissionLogController {
    
-    async listPermissionLogs(req: express.Request, res: express.Response) {
-        const permissionLogs = await permissionLogService.list(100, 0);
-        res.status(200).send(permissionLogs);
+    async listPermissionLogs(req: express.Request, res: express.Response, next: express.NextFunction) {
+        const queryParams = extractParamsFromQuery(req.query)
+
+        if (!queryParams) return res.status(400).send({ message: "Invalid query body properties" })
+        try {
+            const permissionLogs = await permissionLogService.list(queryParams);
+            res.status(200).send(permissionLogs);
+        } catch (error) {
+            next(error)
+        }   
+       
     }
 
     async getPermissionLogById(req: express.Request, res: express.Response) {
