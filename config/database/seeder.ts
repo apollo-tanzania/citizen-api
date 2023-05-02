e166c042ee7a0ffcfad020ea729931be10ae3371
masakachristopher <masakachristopher@yahoo.com>
2023-05-02 04:17:00 +0300

    change mongoose version

--- config/database/seeder.ts
+++ config/database/seeder.ts
@@ -10,6 +10,9 @@ import { PermissionFlag } from "../../common/middleware/common.permissionflag.en
 import Long from 'long'
 import checkIfIs32bitInteger from "../../common/helpers/checkIfIs32bitIntenger";
 import PermissionLogModel from "../../model/permissionLog";
+import LostPhoneReportModel from "../../model/report";
+import ReportVerifcaitonLogModel from "../../model/reportVerificationLog";
+import { AnyArray } from "mongoose";
 // initialize models variables
 const Station = StationModel;
 const User = UserModel;
@@ -17,72 +20,72 @@ const Admin = AdminModel;
 const Permission = PermissionModel;
 const StolenPhone = StolenPhoneModel;
 const PermissionLog = PermissionLogModel;
+const Report = LostPhoneReportModel;
 
-const dataSeeder = async() => {
+
+const dataSeeder = async () => {
 
     // User.db.dropCollection("users")
     // Station.db.dropCollection("stations")
     // StolenPhoneModel.db.dropDatabase();
-    Permission.db.dropCollection("permissions")
-      console.log(await PermissionLogModel.find())
+    // Permission.db.dropCollection("permissions")
+    // console.log(Permission.db.collections)
+    // StolenPhone.db.dropCollection("stolenphones")
+    // Report.db.dropCollection("reports")
+
+    // return
+
+
+    //   console.log(await PermissionLogModel.find())
+    // console.log(await ReportVerifcaitonLogModel.find())
 
 
 
-    Station.estimatedDocumentCount(undefined, (err: any, count: number) => {
+
+    Station.countDocuments((err: Error, count: number) => {
         if (!err && count === 0) {
             new Station({
                 _id: "RB-TBT",
                 name: "Tabata",
                 location: "Tabata"
             })
-                .save((err: any) => {
-                    if (err) {
-                        return console.log('error', err);
-                    }
-                    console.log("Tabata Police station added successfully");
-                })
+                .save()
+                .then((station: any) => console.log(`${station?.name} is created`))
+                .catch((err: any) => console.error(err))
 
             new Station({
                 _id: "RB-MBZ",
                 name: "Mbezi",
                 location: "Mbezi"
             })
-                .save((err: any) => {
-                    if (err) {
-                        return console.log('error', err);
-                    }
-                    console.log("Mbezi Police Station added successfully");
-                })
+                .save()
+                .then((station: any) => console.log(`${station?.name} is created`))
+                .catch((err: any) => console.error(err))
+
 
             new Station({
                 _id: "RB-KMR",
                 name: "Kimara",
                 location: "Kimara"
             })
-                .save((err: any) => {
-                    if (err) {
-                        return console.log('error', err);
-                    }
-                    console.log("Kimara Police station added successfully");
-                })
+                .save()
+                .then((station: any) => console.log(`${station?.name} is created`))
+                .catch((err: any) => console.error(err))
 
             new Station({
                 _id: "RB-CHA",
                 name: "Chamazi",
                 location: "Chamazi"
             })
-                .save((err: any) => {
-                    if (err) {
-                        return console.log('error', err);
-                    }
-                    console.log("Chamazi Police Station added successfully");
-                })
+                .save()
+                .then((station: any) => console.log(`${station?.name} is created`))
+                .catch((err: any) => console.error(err))
 
         }
     })
 
 
-    Permission.estimatedDocumentCount(undefined, async (err: any, count: number) => {
+    Permission.countDocuments(async (err: Error, count: number) => {
         const { startSession } = mongooseService.getMongoose()
         const session = await startSession();
 
@@ -98,7 +101,7 @@ const dataSeeder = async() => {
                             permissionsList.push({
                                 name: key,
                                 genericName: key?.replaceAll("_", " "),
-                                flag: Long.fromNumber(PermissionFlag[key] as unknown as number) ,
+                                flag: Long.fromNumber(PermissionFlag[key] as unknown as number),
                             })
                         }
                     }
@@ -110,12 +113,9 @@ const dataSeeder = async() => {
                         genericName: permission.genericName,
                         flag: permission.flag
                     })
-                        .save((err: any) => {
-                            if (err) {
-                                return console.log('error', err);
-                            }
-                            console.log("Permission: " + permission.name + " created successfully with value: " + permission.flag);
-                        })
+                        .save()
+                        .then((permission: any) => console.log(`${permission?.name} is created`))
+                        .catch((err: any) => console.error(err))
                 })
 
                 await session.commitTransaction();
@@ -130,7 +130,7 @@ const dataSeeder = async() => {
         }
     })
 
-    User.estimatedDocumentCount(undefined, async (err: any, count: number) => {
+    User.countDocuments( async (err: Error, count: number) => {
         const { startSession } = mongooseService.getMongoose()
         const session = await startSession();
 
@@ -149,25 +149,18 @@ const dataSeeder = async() => {
                     password: hashedPassword,
                     confirmed: true
                 })
-                    .save(async (err: any, user: any) => {
-                        if (err) {
-                            return console.log('error', err);
-                        }
-
+                    .save()
+                    .then((user: any) => {
                         new Admin({
                             username: user?._id,
                             permissionFlags: 8589934592
+                            // 8589965312 {create report, view reports}
                         })
-                            .save((err: any, admin: any) => {
-                                if (err) {
-                                    return console.log('error', err);
-                                }
-
-                                console.log("Admin added successfully");
-
-                            })
-
+                            .save()
+                            .then((admin: any) => console.log(`Admin is created`))
+                            .catch((err: any) => console.log(err))
                     })
+                    .catch((err: any) => console.error(err))
             }
             await session.commitTransaction();
 
