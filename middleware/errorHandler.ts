2b0a1fbd80e092935ddc76fab14d5eda3de3a5b0
masakachristopher <masakachristopher@yahoo.com>
2023-05-07 22:54:00 +0300

    add Error to errorHandler

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -3,6 +3,7 @@ import { BadGateWayError } from '../errors/BadGateWayError';
 import { BadRequestError } from '../errors/BadRequestError';
 import { ResourceNotFoundError } from '../errors/ResourceNotFoundError';
 import buildApiResponse from '../common/api/buildApiResponse';
+import { CustomError } from '../errors/CustomError';
 
 function errorHandler(
     error: Error,
@@ -24,6 +25,11 @@ function errorHandler(
     if (error instanceof ResourceNotFoundError) {
         response.locals.error = error
         return buildApiResponse(response, error.status, false, "Error")
+    }
+
+    if (error instanceof CustomError) {
+        response.locals.error = error
+        return buildApiResponse(response, error.status, false)
     }
 
     response.locals.error = error;
