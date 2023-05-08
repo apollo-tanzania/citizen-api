91e4110e3716f55bd19f30126473f7a058d00798
masakachristopher <masakachristopher@yahoo.com>
2023-05-08 13:12:28 +0300

    modify report repository

--- repository/report.ts
+++ repository/report.ts
@@ -3,7 +3,7 @@ import ReportModel from '../model/report';
 import { PatchReportDto } from '../dto/report/patchReport';
 import { PutReportDto } from '../dto/report/putReport';
 import { CreateReportDto } from '../dto/report/createReport';
-import { ClientSession, FilterQuery, UpdateQuery } from 'mongoose';
+import { ClientSession, FilterQuery, Types, UpdateQuery } from 'mongoose';
 import mongooseService from '../common/services/mongoose.service';
 import PhoneModel from '../model/phone';
 import StolenPhoneModel from '../model/stolenPhone';
@@ -109,7 +109,7 @@ class ReportRepository {
             let reportQModelueryFilterArray = []
             let phoneModelQueryFilterArray = []
             type QueryType = Record<any, Number>
-            let query : FilterQuery<QueryType> = {}
+            let query: FilterQuery<QueryType> = {}
 
             if (reportFields.phone.imei1) {
                 query.imei1 = reportFields.phone.imei1
@@ -191,6 +191,101 @@ class ReportRepository {
         }
     }
 
+    // Just a newer version of the create report method with association 
+    // async saveReport(reportFields: CreateReportDto) {
+    //     const { startSession } = mongooseService.getMongoose();
+    //     const session: ClientSession = await startSession();
+
+    //     session.startTransaction(); // Start transaction
+
+
+    //     try {
+
+    //         let reportQModelueryFilterArray = []
+    //         let phoneModelQueryFilterArray = []
+    //         type QueryType = Record<any, Number>
+    //         let query: FilterQuery<QueryType> = {}
+
+    //         if (reportFields.phone.imei1) {
+    //             query.imei1 = reportFields.phone.imei1
+    //             reportQModelueryFilterArray.push({ "phone.imei1": query.imei1 })
+    //             phoneModelQueryFilterArray.push({ "imei1": query.imei1 })
+    //         }
+    //         if (reportFields.phone.imei2) {
+    //             query.imei2 = reportFields.phone.imei2
+    //             reportQModelueryFilterArray.push({ "phone.imei2": query.imei2 })
+    //             phoneModelQueryFilterArray.push({ "imei2": query.imei2 })
+    //         }
+
+    //         if (reportFields.phone.imei3) {
+    //             query.imei3 = reportFields.phone.imei3
+    //             reportQModelueryFilterArray.push({ "phone.imei3": query.imei3 })
+    //             phoneModelQueryFilterArray.push({ "imei3": query.imei3 })
+    //         }
+
+    //         // returns null if not found
+    //         const reportFound = await this.Report.findOne({
+    //             $or: reportQModelueryFilterArray,
+    //         },
+    //             null,
+    //             { sort: { _id: -1 } }
+    //         ).exec();
+
+    //         let report;
+
+    //         report = new this.Report({
+    //             ...reportFields,
+    //         });
+
+    //         if (reportFound?._id) {
+    //             report = new this.Report({
+    //                 ...reportFields,
+    //                 originalReportId: new (mongooseService.getMongoose().Types.ObjectId)(reportFound?._id),
+    //             });
+    //         }
+
+    //         await report.save();
+
+    //         const existingPhone = await this.StolenPhone.findOne({
+    //             $or: phoneModelQueryFilterArray,
+    //         },
+    //             null,
+    //             { sort: { _id: -1 } }
+    //         ).exec();
+
+    //         const update: UpdateQuery<{ countReportedStolenOrLost: number }> = { $inc: { countReportedStolenOrLost: 1 } }
+
+    //         if (existingPhone) {
+    //             await this.StolenPhone.findOneAndUpdate({
+    //                 $or: phoneModelQueryFilterArray
+    //             }, update)
+
+    //         } else {
+    //             const stolenPhone = new this.StolenPhone({
+    //                 ...reportFields?.phone,
+    //                 countReportedStolenOrLost: 1
+    //             })
+
+    //             await stolenPhone.save();
+
+    //         }
+
+
+    //         await session.commitTransaction();
+
+    //         return report;
+
+    //     } catch (error) {
+
+    //         await session.abortTransaction();
+    //         throw error;
+
+    //     } finally {
+    //         session.endSession();
+
+    //     }
+    // }
+
     async removeReportById(reportId: string) {
         return this.Report.findByIdAndRemove({ _id: reportId }).exec();
     }
@@ -215,6 +310,16 @@ class ReportRepository {
 
         return existingReport;
     }
+
+    // TODO
+    // getNumberOfReports(phoneId){
+
+    // }
+
+    // getNumberOfVerifiedReports(phoneId){
+
+    // }
+    //
 
     async approveReport(
         reportId: string,
@@ -330,56 +435,41 @@ class ReportRepository {
 
     }
 
-    // async addReportVerificationLog(lawEnforcementOfficerVerificationHistoryFields: CreateLawEnforcementVerificationHistoryDto) {
-    //     const session: ClientSession = await mongooseService.getMongoose().startSession();
+    // Patterns and Trends
+    // 1. phone theft activity
+    private async phoneTheftActivity() {
+        try {
 
-    //     session.startTransaction(); // Start transaction
+            const stolenPhones = await this.StolenPhone.find().exec();
 
-    //     try {
+            let totalReports = 0
+            let totalVerifiedReports = 0
 
-    //         const officerToBeVerified = await this.LawEnforcement.findOne({ username: lawEnforcementOfficerVerificationHistoryFields.lawEnforcementId });
+            for (let i = 0; i < stolenPhones.length; i++) {
+                let reportsFoundWithTheIMEI = await this.Report.find({
+                    $or: [
+                        { "phone.imei1": stolenPhones[i].imei1 },
+                        { "phone.imei2": stolenPhones[i].imei1 },
+                        { "phone.imei3": stolenPhones[i].imei1 },
+                    ]
+                }).exec()
 
-    //         // return officerToBeVerified;
-    //         if (officerToBeVerified?.isVerified) {
-    //             return {
-    //                 message: "Officer already verified",
-    //                 type: "Conflict"
-    //             }
-    //         }
+                totalReports += stolenPhones[i].countReportedStolenOrLost
+                totalVerifiedReports += reportsFoundWithTheIMEI.filter(report => report.verified === true).length
+            }
 
-    //         // update status to verified
-    //         await this.LawEnforcement.findOneAndUpdate(
-    //             { username: officerToBeVerified?.username },
-    //             {
-    //                 $set: {
-    //                     isVerified: true
-    //                 }
-    //             },
-    //             { new: true }
-    //         ).exec();
+            const verifiedRatio = totalReports > 0 ? totalVerifiedReports / totalReports : 0
 
-    //         const lawEnforcementOfficerVerificationHistory = await new this.LawEnforcementVerificationHistory({
-    //             ...lawEnforcementOfficerVerificationHistoryFields
-    //         })
+            return {
+                totalReports,
+                totalVerifiedReports,
+                verifiedRatio
+            }
 
-    //         await lawEnforcementOfficerVerificationHistory.save();
-
-    //         await session.commitTransaction();
-
-    //         return lawEnforcementOfficerVerificationHistory;
-
-    //     } catch (error) {
-    //         // Rollback the transaction i.e rollback any changes made to the database
-    //         await session.abortTransaction();
-    //         // throw error;
-    //         return error;
-
-    //     } finally {
-    //         // Ending sesion
-    //         session.endSession()
-    //     }
-
-    // }
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
 export default new ReportRepository();
