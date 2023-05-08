08a3ba901e94b63dbb7a2ad73833bc20ef656279
masakachristopher <masakachristopher@yahoo.com>
2023-05-08 12:41:06 +0300

    create file

---
+++ common/helpers/constants.ts
@@ -0,0 +1,12 @@
+export const strings = Object.freeze({
+    REPORT_REMARK_PHRASE: {
+        // if the phone has never been reported stolen before
+        PHRASE1: 'This phone never been reported stolen before',
+        // if the phone has been reported stolen once 
+        PHRASE2: 'This phone has been reported stolen once before',
+        // if the phone has been reported stolen multiple times
+        PHRASE3: 'This phone has been reported stolen a few times before',
+        // if the phone has been reported stolen many times (6 or more times)
+        PHRASE4: 'This phone has a history of being reported stolen'
+    }
+})
